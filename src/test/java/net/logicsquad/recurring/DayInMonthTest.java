6f3176449c4ee16ac4c4d22d935cb99de727ce52
Paul Hoadley <paulh@logicsquad.net>
2018-06-16 19:11:40 +0930

    Add Javadoc comments.

--- src/test/java/net/logicsquad/recurring/DayInMonthTest.java
+++ src/test/java/net/logicsquad/recurring/DayInMonthTest.java
@@ -9,13 +9,30 @@ import java.time.temporal.TemporalAdjusters;
 
 import org.junit.Test;
 
+/**
+ * Unit tests on {@link DayInMonth}.
+ * 
+ * @author paulh
+ */
 public class DayInMonthTest {
+	/**
+	 * Second Monday in month
+	 */
 	private DayInMonth exp = DayInMonth.of(DayOfWeek.MONDAY, 2);
 
+	/**
+	 * Second Monday in June 2018
+	 */
 	private LocalDate match1 = LocalDate.of(2018, 6, 11);
 
+	/**
+	 * Second Monday in August 2018
+	 */
 	private LocalDate match2 = LocalDate.of(2018, 8, 13);
 
+	/**
+	 * Tests that expression matches known-good dates.
+	 */
 	@Test
 	public void expressionMatchesSecondMondays() {
 		assertTrue(exp.includes(match1));
@@ -23,6 +40,9 @@ public class DayInMonthTest {
 		return;
 	}
 
+	/**
+	 * Tests that expression matches no <em>other</em> dates in month.
+	 */
 	@Test
 	public void expressionDoesNotMatchOtherDates() {
 		LocalDate date = LocalDate.of(2018, 6, 1);
